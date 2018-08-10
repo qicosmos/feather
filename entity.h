0e8e433aaf1b1d1bc64d5c58266fa9fb72ba7968
qicosmos <qicosmos@163.com>
2018-08-10 18:58:17 +0800

    update entity and html

--- entity.h
+++ entity.h
@@ -6,6 +6,47 @@
 #define FEATHER_ENTITY_H
 
 namespace feather{
+	struct pp_posts {
+		int ID; 
+		int post_author; 
+		std::string post_date; 
+		std::string post_title; 
+		std::string post_content;
+		std::string post_status; 
+		std::string post_modified; 
+		std::string content_abstract; 
+		std::string url;
+	};
+	REFLECTION(pp_posts, ID, post_author, post_date, post_title, post_content, post_status, post_modified, content_abstract, url);
+
+	struct pp_user {
+		int ID; 
+		std::string user_login; 
+		std::string user_nickname; 
+		std::string user_email; 
+		std::string user_registered; 
+		std::string user_icon;
+	};
+	REFLECTION(pp_user, ID, user_login, user_nickname, user_email, user_registered, user_icon);
+
+	struct pp_comment {
+		int ID; 
+		int user_id; 
+		int post_id;
+		int comment_parant;
+		std::string comment_content; 
+		std::string comment_date;
+	};
+	REFLECTION(pp_comment, ID, user_id, post_id, comment_parant, comment_content, comment_date);
+
+	struct pp_post_views {
+		int ID; 
+		int type; 
+		int count; 
+		std::string period;
+	};
+	REFLECTION(pp_post_views, ID, type, count, period);
+
     struct user{
         int id;
         std::string		user_name;
