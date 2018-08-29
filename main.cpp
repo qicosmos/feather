fe58e4df65ce0ffbc6a318811d68d4a444ed1552
qicosmos <qicosmos@163.com>
2018-08-29 19:59:50 +0800

    add permission for download

--- main.cpp
+++ main.cpp
@@ -89,6 +89,7 @@ int main(){
     init(cfg);
 
     cinatra::http_server server(cfg.thread_num);
+	server.set_public_root_directory("purecpp");
     server.set_static_dir("./static/");
 	server.enable_http_cache(false);//set global cache
     bool r = server.listen("0.0.0.0", cfg.port);
