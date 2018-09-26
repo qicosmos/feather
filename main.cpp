1167a75f1b1d1902422718d113e73e5e64c2797d
qicosmos <qicosmos@163.com>
2018-09-26 05:46:53 -0700

    clean

--- main.cpp
+++ main.cpp
@@ -6,10 +6,6 @@
 #include "feather_cfg.hpp"
 #include "entity.h"
 #include "dao.hpp"
-#include "user_controller.hpp"
-#include "article_controller.hpp"
-#include "upload_controller.hpp"
-#include "static_res_controller.hpp"
 #include "purecpp_controller.hpp"
 #include "validate.hpp"
 
@@ -17,86 +13,32 @@ using namespace feather;
 using namespace ormpp;
 using namespace cinatra;
 
-void init_db(bool clear_all){
-    dao_t<dbng<mysql>> dao;
-    bool r = true;
-    if(clear_all){
-        bool r1 = dao.drop_table<user>();
-        bool r2 = dao.drop_table<article>();
-        bool r3 = dao.drop_table<article_detail>();
-        assert(r1&&r2&&r3);
-    }
+void init(const feather_cfg& cfg) {
 
-    {
-        bool r1 = dao.create_table<user>({ SID(user::id) });
-        bool r2 = dao.create_table<article>({ SID(article::id) });
-        bool r3 = dao.create_table<article_detail>({ SID(article_detail::id) });
-        assert(r1&&r2&&r3);
-    }
+	nanolog::initialize(nanolog::GuaranteedLogger(), cfg.log_path, cfg.log_name, cfg.roll_file_size);
 
-//    {
-//        //add some test data to tables
-//        user u1{0, "tom"};
-//        user u2{0, "mike"};
-//        std::vector<user> v{u1, u2};
-//        int n = dao.add_objects(v);
-//        assert(n==2);
-//        bool r1 = dao.add_object(u1);
-//        bool r2 = dao.add_object(u2);
-//    }
-//
-//    {
-//        article ar1{0, "title1", "test", 1, 1, get_cur_time_str()};
-//        article ar2{0, "title2", "hello", 2, 1, get_cur_time_str()};
-//        bool r1 = dao.add_object(ar1);
-//        bool r2 = dao.add_object(ar2);
-//        assert(r1&&r2);
-//
-//        article_detail detail1{0, ar1.id, "title1", "it is a test", get_cur_time_str()};
-//        article_detail detail2{0, ar2.id, "title2", "hello world", get_cur_time_str()};
-//        r1 = dao.add_object(detail1);
-//        r2 = dao.add_object(detail2);
-//        assert(r1&&r2);
-//    }
-//
-//    {
-//        std::vector<user> v;
-//        bool r = dao.get_object(v, "id<3");
-//        assert(r);
-//
-//        std::vector<article> v1;
-//        bool r1 = dao.get_object(v1);
-//        assert(r1);
-//    }
+	dao_t<dbng<mysql>>::init(cfg.db_conn_num, cfg.db_ip.data(), cfg.user_name.data(), cfg.pwd.data(),
+		cfg.db_name.data(), cfg.timeout);
 }
 
-void init(const feather_cfg& cfg){
+int main() {
+	feather_cfg cfg{};
+	bool ret = config_manager::from_file(cfg, "./cfg/feather.cfg");
+	if (!ret) {
+		return -1;
+	}
 
-    nanolog::initialize(nanolog::GuaranteedLogger(), cfg.log_path, cfg.log_name, cfg.roll_file_size);
+	init(cfg);
 
-    dao_t<dbng<mysql>>::init(cfg.db_conn_num, cfg.db_ip.data(), cfg.user_name.data(), cfg.pwd.data(),
-                             cfg.db_name.data(), cfg.timeout);
-    init_db(cfg.drop_all_table);
-}
-
-int main(){
-    feather_cfg cfg{};
-    bool ret = config_manager::from_file(cfg, "./cfg/feather.cfg");
-    if (!ret) {
-        return -1;
-    }
-
-    init(cfg);
-
-    cinatra::http_server server(cfg.thread_num);
+	cinatra::http_server server(cfg.thread_num);
 	server.set_res_cache_max_age(86400);
 	server.set_public_root_directory("purecpp");
-    server.set_static_dir("static");
+	server.set_static_dir("static");
 	server.enable_http_cache(false);//set global cache
-    bool r = server.listen("0.0.0.0", cfg.port);
-    if (!r) {
-        LOG_INFO << "listen failed";
-        return -1;
+	bool r = server.listen("0.0.0.0", cfg.port);
+	if (!r) {
+		LOG_INFO << "listen failed";
+		return -1;
 	}
 
 	purecpp_controller purecpp_ctl;
@@ -116,7 +58,7 @@ int main(){
 
 	server.set_http_handler<POST>("/comment", &purecpp_controller::comment, &purecpp_ctl, check_login{}, check_comment_input{});
 	server.set_http_handler<GET, POST>("/remove_comment", &purecpp_controller::remove_comment, &purecpp_ctl, check_login{}, check_remove_comment_input{});
-	
+
 	server.set_http_handler<GET, POST>("/add_post_page", &purecpp_controller::add_post_page, &purecpp_ctl, check_login{});
 	server.set_http_handler<GET, POST>("/add_post", &purecpp_controller::add_post, &purecpp_ctl, check_login{});
 	server.set_http_handler<GET, POST>("/remove_post", &purecpp_controller::remove_post, &purecpp_ctl, check_login{}, check_edit_post_input{});
@@ -126,21 +68,7 @@ int main(){
 	server.set_http_handler<GET, POST>("/my_post", &purecpp_controller::my_post, &purecpp_ctl, check_login{}, check_start_end_input{});
 	server.set_http_handler<GET, POST>("/upload_file", &purecpp_controller::upload, &purecpp_ctl, check_login{});
 
-    //user_controller user_ctl;
-    //server.set_http_handler<POST>("/add_user", &user_controller::add_user, &user_ctl);
+	server.run();
 
-    //article_controller article_ctl;
-    //server.set_http_handler<GET, POST>("/", &article_controller::index, &article_ctl);
-    //server.set_http_handler<POST>("/add_article", &article_controller::add_article, &article_ctl);
-    //server.set_http_handler<GET, POST>("/get_article_list", &article_controller::get_article_list, &article_ctl);
-    //server.set_http_handler<GET, POST>("/get_article_detail", &article_controller::get_article_detail, &article_ctl);
-    //server.set_http_handler<GET, POST>("/remove_article", &article_controller::remove_article, &article_ctl);
-    //server.set_http_handler<POST>("/update_article", &article_controller::update_article, &article_ctl);
-
-    //upload_controller up;
-    //server.set_http_handler<POST>("/upload", &upload_controller::upload, &up);
-
-    server.run();
-
-    return 0;
+	return 0;
 }
